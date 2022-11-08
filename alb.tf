8636b0a3dca0986d04fb10f2b35a03351b483de8
Prakash Rao <116753469+coolguy0704@users.noreply.github.com>
2022-11-08 11:36:56 +0900

    Update alb.tf
    add ingress and egress rules for alb sg

--- alb.tf
+++ alb.tf
@@ -16,4 +16,16 @@ resource "aws_lb" "for_webserver" {
 resource "aws_security_group" "alb" {
     name ="alb"
     vpc_id= aws_vpc.example.id
+    ingress{
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+   egress{
+       from_port  = 0
+       to_port    = 0
+       protocol   = "-1"
+       cidr_blocks=["0.0.0.0/0"]
+   }
 }
