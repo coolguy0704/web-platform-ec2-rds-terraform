c65009934ecebc720ee5b6fe2a201781903c0da9
Prakash Rao <116753469+coolguy0704@users.noreply.github.com>
2022-11-08 11:25:20 +0900

    Update vpc.tf
    associate routes with public subnet

--- vpc.tf
+++ vpc.tf
@@ -46,3 +46,20 @@ resource "aws_route" "public" {
     gateway_id     = aws_internet_gateway.example.id
     destination_cidr_block = "0.0.0.0/0" 
 }
+
+resource "aws_route_table_association" "public_a" {
+    subnet_id      = aws_subnet.public_a.id
+    route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_c" {
+    subnet_id      = aws_subnet.public_c.id
+    route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_a_web" {
+    subnet_id      = aws_subnet.public_a_web.id
+    route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_c_web" {
+    subnet_id      = aws_subnet.public_c_web.id
+    route_table_id = aws_route_table.public.id
+}
