a0ae04757e615a09b644161f1accfa22bbde73de
Prakash Rao <116753469+coolguy0704@users.noreply.github.com>
2022-11-08 11:32:39 +0900

    Update ec2-app.tf
    add ingress and egress rules

--- ec2-app.tf
+++ ec2-app.tf
@@ -42,4 +42,16 @@ resource "aws_instance" "c" {
 resource "aws_security_group" "for_webserver_ec2" {
     name ="for-ec2-instances"
     vpc_id= aws_vpc.example.id
+    ingress{
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+        security_groups =[aws_security_group.alb.id]
+    }
+    egress{
+       from_port  = 0
+       to_port    = 0
+       protocol   = "-1"
+       cidr_blocks=["0.0.0.0/0"]
+   }
 }
